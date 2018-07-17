05e7603df825938ae762768a8f775d2a84fc7b71
younginsong21 <younginsong21@naver.com>
2018-07-17 21:54:03 +0900

    update models

--- Highlight_Tekken/codes/models/model.py
+++ Highlight_Tekken/codes/models/model.py
@@ -9,7 +9,8 @@ import torch.utils.data
 
 from c3d import C3D
 from videoloader import get_loader
-from lstm import bi_LSTM
+from bidir_lstm import GRU
+# from single_lstm import single_LSTM
 
 h_video_path = '../../dataset/HV'
 r_video_path = '../../dataset/RV'
@@ -20,52 +21,74 @@ class HighlightClassifier(nn.Module):
     def __init__(self):
         super(HighlightClassifier, self).__init__()
         # define C3D layer
-        self.c3d = C3D()
-        self.c3d.load_state_dict(torch.load(weight_path)) # load pre-trained weight
-        self.c3d = self.c3d.cuda()
+        c3d = C3D()
+        c3d.load_state_dict(torch.load(weight_path)) # load pre-trained weight
+        c3d = c3d.cuda()
 
         # remove c3d fc layers
-        fc_removed = list(self.c3d.children())[:-6]
-        self.c3d = nn.Sequential(*fc_removed)
+        fc_removed = list(c3d.children())[:-6]
 
-        # define bidirectional LSTM layer
-        self.lstm = bi_LSTM()
-        self.lstm = self.lstm.cuda()
+        _p3d_net = []
+        relu = nn.ReLU()
+
+        for layer in fc_removed:
+            for param in layer.parameters():
+                param.requires_grad = False    # no training
+            if layer.__class__.__name__ == 'MaxPool3d':
+                _p3d_net.extend([layer, relu]) # add activation function
+            else:
+                _p3d_net.append(layer)
+
+        c3d = nn.Sequential(*_p3d_net).cuda()  # new p3d net
+
+        self.gru = GRU(c3d)
+        self.gru = self.gru.cuda()
+
+        # # define bidirectional LSTM layer
+        # self.lstm = bi_LSTM()
+        # self.lstm = self.lstm.cuda()
 
     def forward(self, x):
-        # input = [B, D, C, H, W]
-        win_start = 0
-        win_end = 48
-        win_out = []
-
-        x = x.permute(0, 2, 1, 3, 4) # => x:[B, C, D, H, W]
-        n_snippets = 0
-
-        # C3D layer forward (unit: 48 frames length snippet)
-        while win_end < x.shape[2]:
-            if n_snippets == 3:
-                break           # out of memory problem
-
-            n_snippets += 1
-
-            snippet = x[:, :, win_start:win_end, :, :] # torch.Size([1, 3, 48, 256, 256])
-            snippet_out = self.c3d(snippet) # forwarding to C3D layer
-            snippet_out = snippet_out.squeeze() # torch.Size([512, 3, 9, 9])
-            snippet_out = snippet_out.view(-1, 512 * 3 * 9 * 9) # flatten to 1d -> torch.Size([1, 124416])
-
-            win_out.append(snippet_out)
-
-            # sliding window
-            win_start += 6
-            win_end += 6
-
-        c3d_out = torch.cat(win_out, dim=0) # torch.Size([n_snippets=3, 124416])
-
-        # bi-direactional LSTM forward
-        lstm_out =self.lstm(c3d_out)
-        print(lstm_out)
-        out = torch.argmax(lstm_out, dim=1) # tensor([ 0,  0,  0,  0,  0,  1], device='cuda:0')
+        out = self.gru(x)
         return out
+
+        # # input = [B, D, C, H, W]
+        # win_start = 0
+        # win_end = 48
+        # win_out = []
+        #
+        # x = x.permute(0, 2, 1, 3, 4) # => x:[B, C, D, H, W]
+        # n_snippets = 0
+        #
+        # # C3D layer forward (unit: 48 frames length snippet)
+        # while win_end < x.shape[2]:
+        #     # if n_snippets == 3:
+        #     #     break           # out of memory problem
+        #
+        #     n_snippets += 1
+        #
+        #     snippet = x[:, :, win_start:win_end, :, :] # torch.Size([1, 3, 48, 256, 256])
+        #     c3d_out = self.c3d(snippet) # forwarding to C3D layer
+        #     c3d_out = c3d_out.squeeze() # torch.Size([512, 3, 9, 9])
+        #
+        #     lstm_out = self.lstm(c3d_out)
+        #
+        #
+        #     # snippet_out = snippet_out.view(-1, 512 * 3 * 9 * 9) # flatten to 1d -> torch.Size([1, 124416])
+        #     #
+        #     # win_out.append(snippet_out)
+        #     #
+        #     # # sliding window
+        #     # win_start += 6
+        #     # win_end += 6
+        #
+        # # c3d_out = torch.cat(win_out, dim=0) # torch.Size([n_snippets=3, 124416])
+        # #
+        # # # bi-direactional LSTM forward
+        # # lstm_out =self.lstm(c3d_out)
+        # # print(lstm_out)
+        # # out = torch.argmax(lstm_out, dim=1) # tensor([ 0,  0,  0,  0,  0,  1], device='cuda:0')
+        # # return out
 
 
 if __name__ == '__main__':
--- Highlight_Tekken/codes/models/single_lstm.py
+++ Highlight_Tekken/codes/models/single_lstm.py
@@ -4,16 +4,16 @@ import torch
 # Device configuration
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-class bi_LSTM(nn.Module):
+class single_LSTM(nn.Module):
     def __init__(self, n_classes=2, input_size=512 * 3 * 9 * 9, hidden_size=2, n_layers=1):
-        super(bi_LSTM, self).__init__()
+        super(single_LSTM, self).__init__()
 
         self.n_classes = n_classes
         self.n_layers = n_layers
         self.input_size = input_size
         self.hidden_size = hidden_size
 
-        self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True, bidirectional=True)
+        self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True, bidirectional=False) # one direction
 
     # input size = [n_snippet, 512 * 3 * 9 * 9]
     # sequence length = n_snippet
@@ -22,8 +22,8 @@ class bi_LSTM(nn.Module):
 
         n_batch = x.size(0)
 
-        h_0 = torch.zeros(self.n_layers*2, n_batch, self.hidden_size).to(device) # 2 for bidirectional
-        c_0 = torch.zeros(self.n_layers*2, n_batch, self.hidden_size).to(device) # 2 for bidirectional
+        h_0 = torch.zeros(self.n_layers*1, n_batch, self.hidden_size).to(device)
+        c_0 = torch.zeros(self.n_layers*1, n_batch, self.hidden_size).to(device)
 
         # Propagate input through lstm
         out, _ = self.lstm(x, (h_0, c_0))
